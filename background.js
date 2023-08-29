8922fa886dba1abb63dc0ca000ad389290f14266
PowerOf <krajnyik.richard@gmail.com>
2023-08-29 03:23:58 +0100

    added more logging

--- background.js
+++ background.js
@@ -1,21 +1,29 @@
+console.log('Background service worker script loaded.');
+
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-      id: "saveBookmark",
-      title: "Save Bookmark",
-      contexts: ["link"]
+  console.log('Extension installed. Setting up context menu.');
+  chrome.contextMenus.create({
+    id: "saveBookmark",
+    title: "Save Bookmark",
+    contexts: ["link"]
+  });
+  console.log('Context menu setup complete.');
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  console.log('Context menu item clicked.');
+  if (info.menuItemId === "saveBookmark") {
+    console.log('Save Bookmark selected.');
+    const url = info.linkUrl;
+    const title = info.selectionText || 'Untitled';
+    console.log(`URL: ${url}, Title: ${title}`);
+
+    chrome.bookmarks.create({
+      'parentId': "1",
+      'title': title,
+      'url': url
+    }, function(bookmark) {
+      console.log('Bookmark created:', bookmark);
     });
-  });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "saveBookmark") {
-      const url = info.linkUrl;
-      const title = info.selectionText || 'Untitled';
-  
-      chrome.bookmarks.create({
-        'parentId': "1", // 1 is the ID for the bookmarks bar, you can adjust as needed
-        'title': title,
-        'url': url
-      });
-    }
-  });
-  
+  }
+});
